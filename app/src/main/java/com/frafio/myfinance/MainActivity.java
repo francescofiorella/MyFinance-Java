557d4e4c88a0708c1fb908caa079d843b40e5ba2
francesco_fiorella <francescofio99@gmail.com>
2021-03-31 20:48:33 +0200

    Commit iniziale, firebase collegato

---
+++ app/src/main/java/com/frafio/myfinance/MainActivity.java
@@ -0,0 +1,14 @@
+package com.frafio.myfinance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
