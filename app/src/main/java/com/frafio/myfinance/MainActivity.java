c20a2e9c057493d45290d62c461789fa2e1c9fc9
francesco_fiorella <francescofio99@gmail.com>
2021-04-04 19:33:30 +0200

    bottom nav bar fixed, profile initialized

--- app/src/main/java/com/frafio/myfinance/MainActivity.java
+++ app/src/main/java/com/frafio/myfinance/MainActivity.java
@@ -6,19 +6,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.frafio.myfinance.fragments.HomeFragment;
+import com.frafio.myfinance.fragments.ListFragment;
+import com.frafio.myfinance.fragments.ProfileFragment;
+import com.frafio.myfinance.fragments.SettingsFragment;
 import com.frafio.myfinance.objects.User;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
@@ -37,11 +45,13 @@ public class MainActivity extends AppCompatActivity {
     Typeface nunito;
 
     MaterialToolbar mToolbar;
-    TextView mUserNameTv, mEmailTv;
-    MaterialButton mLoginBtn;
+    BottomNavigationView mBottomNavigationView;
     FloatingActionButton mAddBtn;
 
     FirebaseAuth fAuth;
+
+    // 0 home, 1 list, 2 profile, 3 settings
+    int currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +66,58 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
 
         // collegamento view
-        mUserNameTv = findViewById(R.id.main_username_tv);
-        mEmailTv = findViewById(R.id.main_email_tv);
-        mLoginBtn = findViewById(R.id.loginBtn);
+        mBottomNavigationView = findViewById(R.id.main_bottomNavView);
         mAddBtn = findViewById(R.id.main_addBtn);
 
+        if (savedInstanceState == null) {
+            mToolbar.setTitle("MyFinance");
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frameLayout, new HomeFragment()).commit();
+            currentFragment = 0;
+        }
+
         updateCurrentUser();
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment mFragmentToSet = null;
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        if (currentFragment != 0) {
+                            mToolbar.setTitle("MyFinance");
+                            mFragmentToSet = new HomeFragment();
+                            currentFragment = 0;
+                        }
+                        break;
+                    case R.id.list:
+                        if (currentFragment != 1) {
+                            mToolbar.setTitle("Lista");
+                            mFragmentToSet = new ListFragment();
+                            currentFragment = 1;
+                        }
+                        break;
+                    case R.id.profile:
+                        if (currentFragment != 2) {
+                            mToolbar.setTitle("Profilo");
+                            mFragmentToSet = new ProfileFragment();
+                            currentFragment = 2;
+                        }
+                        break;
+                    case R.id.settings:
+                        if (currentFragment != 3) {
+                            mToolbar.setTitle("Impostazioni");
+                            mFragmentToSet = new SettingsFragment();
+                            currentFragment = 3;
+                        }
+                        break;
+                }
+                if (mFragmentToSet != null) {
+                    getSupportFragmentManager().beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .replace(R.id.main_frameLayout, mFragmentToSet).commit();
+                }
+                return true;
+            }
+        });
     }
 
     private void updateCurrentUser() {
@@ -69,8 +125,6 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser fUser = fAuth.getCurrentUser();
 
         if (fUser != null) {
-            mUserNameTv.setVisibility(View.VISIBLE);
-            mEmailTv.setVisibility(View.VISIBLE);
             if (CURRENTUSER == null) {
                 FirebaseFirestore fStore = FirebaseFirestore.getInstance();
                 fStore.collection("users").document(fUser.getUid()).get()
@@ -78,8 +132,6 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(DocumentSnapshot documentSnapshot) {
                         CURRENTUSER = documentSnapshot.toObject(User.class);
-                        mUserNameTv.setText(CURRENTUSER.getFullName());
-                        mEmailTv.setText(CURRENTUSER.getEmail());
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
@@ -87,31 +139,14 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("LOG", "Error! " + e.getLocalizedMessage());
                     }
                 });
-            } else {
-                mUserNameTv.setText(CURRENTUSER.getFullName());
-                mEmailTv.setText(CURRENTUSER.getEmail());
             }
-            mLoginBtn.setText("Log Out");
-            mLoginBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    FirebaseAuth.getInstance().signOut();
-                    showSnackbar("Logout effettuato!");
-                    updateCurrentUser();
-                }
-            });
         } else {
             CURRENTUSER = null;
-            mUserNameTv.setVisibility(View.GONE);
-            mEmailTv.setVisibility(View.GONE);
-            mLoginBtn.setText("Log In");
-            mLoginBtn.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivityForResult(new Intent(getApplicationContext(), LoginActivity.class), 1);
-                }
-            });
         }
+    }
+
+    public void goToLogin() {
+        startActivityForResult(new Intent(getApplicationContext(), LoginActivity.class), 1);
     }
 
     @Override
@@ -121,12 +156,15 @@ public class MainActivity extends AppCompatActivity {
             boolean userRequest = data.getBooleanExtra("com.frafio.myfinance.userRequest", false);
             if (userRequest) {
                 updateCurrentUser();
+                mToolbar.setTitle("Profilo");
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_frameLayout, new ProfileFragment()).commit();
+                currentFragment = 2;
                 showSnackbar("Hai effettuato l'accesso come " + fAuth.getCurrentUser().getDisplayName());
             }
         }
     }
 
-    private void showSnackbar(String string) {
+    public void showSnackbar(String string) {
         Snackbar snackbar = Snackbar.make(layout, string, BaseTransientBottomBar.LENGTH_SHORT).setAnchorView(mAddBtn)
                 .setBackgroundTint(ContextCompat.getColor(getApplicationContext(), R.color.snackbar))
                 .setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.inverted_primary_text));
---
+++ app/src/main/java/com/frafio/myfinance/fragments/ProfileFragment.java
@@ -0,0 +1,111 @@
+package com.frafio.myfinance.fragments;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.frafio.myfinance.LoginActivity;
+import com.frafio.myfinance.MainActivity;
+import com.frafio.myfinance.R;
+import com.frafio.myfinance.objects.User;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfileFragment extends Fragment {
+
+    ImageView mUserImage;
+    TextView mUserNameTv, mEmailTv;
+    MaterialButton mLoginBtn;
+
+    FirebaseAuth fAuth;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        // collegamento view
+        mUserImage = view.findViewById(R.id.profile_propic_iv);
+        mUserNameTv = view.findViewById(R.id.profile_username_tv);
+        mEmailTv = view.findViewById(R.id.profile_email_tv);
+        mLoginBtn = view.findViewById(R.id.profile_loginBtn);
+
+        setUserData();
+
+        return view;
+    }
+
+    private void setUserData() {
+        fAuth = FirebaseAuth.getInstance();
+        FirebaseUser fUser = fAuth.getCurrentUser();
+
+        if (fUser != null) {
+            mUserNameTv.setVisibility(View.VISIBLE);
+            if (MainActivity.CURRENTUSER == null) {
+                FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+                fStore.collection("users").document(fUser.getUid()).get()
+                        .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                MainActivity.CURRENTUSER = documentSnapshot.toObject(User.class);
+                                mUserNameTv.setText(MainActivity.CURRENTUSER.getFullName());
+                                mEmailTv.setText(MainActivity.CURRENTUSER.getEmail());
+                                if (!MainActivity.CURRENTUSER.getImage().equals("")) {
+                                    Glide.with(getContext()).load(MainActivity.CURRENTUSER.getImage()).apply(RequestOptions.circleCropTransform()).into(mUserImage);
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("LOG", "Error! " + e.getLocalizedMessage());
+                    }
+                });
+            } else {
+                mUserNameTv.setText(MainActivity.CURRENTUSER.getFullName());
+                mEmailTv.setText(MainActivity.CURRENTUSER.getEmail());
+                if (!MainActivity.CURRENTUSER.getImage().equals("")) {
+                    Glide.with(getContext()).load(MainActivity.CURRENTUSER.getImage()).apply(RequestOptions.circleCropTransform()).into(mUserImage);
+                }
+            }
+            mLoginBtn.setText("Log Out");
+            mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FirebaseAuth.getInstance().signOut();
+                    ((MainActivity)getActivity()).showSnackbar("Logout effettuato!");
+                    setUserData();
+                }
+            });
+        } else {
+            MainActivity.CURRENTUSER = null;
+            mUserImage.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_user));
+            mUserNameTv.setVisibility(View.GONE);
+            mEmailTv.setText("Accedi al tuo account");
+            mLoginBtn.setText("Accedi");
+            mLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ((MainActivity)getActivity()).goToLogin();
+                }
+            });
+        }
+    }
+}
